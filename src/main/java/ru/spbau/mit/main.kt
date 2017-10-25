da8dd84e7e4462141b0ef3b6b0ef328c1c3f83e7
niksaz <nikitasazanovich@gmail.com>
2017-10-25 21:53:33 +0300

    Implemented 701E from Codeforces.

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,98 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.io.*
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class Solver(
+    private val k: Int,
+    private val universities: BooleanArray,
+    private val graph: List<Vertex>) {
+
+    private val subtreeUniversityCount = IntArray(universities.size)
+    private val maxSubtreeUniversityCount = Array(universities.size, { Pair(0, 0) } )
+
+    fun solve(): Long {
+        computeSubtreeUniversityCount(0, 0)
+        val root = findOptimalVertex(0)
+        return sumFromRoot(root, root, 0)
+    }
+
+    private fun computeSubtreeUniversityCount(vertex: Int, parent: Int): Int {
+        if (universities[vertex]) {
+            subtreeUniversityCount[vertex]++
+        }
+        for (toVertex in graph[vertex].edges) {
+            if (toVertex != parent) {
+                val childCount = computeSubtreeUniversityCount(toVertex, vertex)
+                subtreeUniversityCount[vertex] += childCount
+                if (childCount > maxSubtreeUniversityCount[vertex].first) {
+                    maxSubtreeUniversityCount[vertex] = Pair(childCount, toVertex)
+                }
+            }
+        }
+        return subtreeUniversityCount[vertex]
+    }
+
+    private fun findOptimalVertex(vertex: Int): Int {
+        return if (maxSubtreeUniversityCount[vertex].first > k) {
+            findOptimalVertex(maxSubtreeUniversityCount[vertex].second)
+        } else {
+            vertex
+        }
+    }
+
+    private fun sumFromRoot(vertex: Int, parent: Int, distanceToRoot: Int): Long {
+        return (if (universities[vertex]) distanceToRoot.toLong() else 0L) +
+            graph[vertex].edges
+            .filter { it != parent }
+            .map { sumFromRoot(it, vertex, distanceToRoot + 1) }
+            .sum()
+    }
+}
+
+data class Vertex(val edges: MutableList<Int> = mutableListOf())
+
+class InputReader(stream: InputStream) {
+    private val reader: BufferedReader = BufferedReader(InputStreamReader(stream), 32768)
+    private var tokenizer: StringTokenizer? = null
+
+    private fun next(): String {
+        while (tokenizer == null || !tokenizer!!.hasMoreTokens()) {
+            try {
+                tokenizer = StringTokenizer(reader.readLine())
+            } catch (e: IOException) {
+                throw RuntimeException(e)
+            }
+        }
+        return tokenizer!!.nextToken()
+    }
+
+    fun nextInt(): Int {
+        return Integer.parseInt(next())
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val reader = InputReader(System.`in`)
+
+    val n = reader.nextInt()
+    val graph = mutableListOf<Vertex>()
+    for (i in 0 until n) {
+        graph.add(Vertex())
+    }
+    val k = reader.nextInt()
+    val isUniversity = BooleanArray(n)
+    for (i in 0 until 2 * k) {
+        val universityIndex = reader.nextInt() - 1
+        isUniversity[universityIndex] = true
+    }
+    for (i in 0 until n - 1) {
+        val edgeA = reader.nextInt() - 1
+        val edgeB = reader.nextInt() - 1
+        graph[edgeA].edges.add(edgeB)
+        graph[edgeB].edges.add(edgeA)
+    }
+
+    val solver = Solver(k, isUniversity, graph.toList())
+    println(solver.solve())
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,53 @@
 package ru.spbau.mit
+
 import kotlin.test.assertEquals
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun exampleTest1() {
+        val n = 7
+        val k = 2
+        val isUniversity = booleanArrayOf(true, true, false, false, true, true, false)
+        val graph = initGraph(n)
+        addEdge(graph, 0, 2)
+        addEdge(graph, 2, 1)
+        addEdge(graph, 3, 4)
+        addEdge(graph, 2, 6)
+        addEdge(graph, 3, 2)
+        addEdge(graph, 3, 5)
+        val solver = Solver(k, isUniversity, graph)
+        assertEquals(6, solver.solve())
+    }
+
+    @Test
+    fun exampleTest2() {
+        val n = 9
+        val k = 3
+        val isUniversity = booleanArrayOf(true, true, true, false, true, true, false, false, true)
+        val graph = initGraph(n)
+        addEdge(graph, 7, 8)
+        addEdge(graph, 2, 1)
+        addEdge(graph, 1, 6)
+        addEdge(graph, 2, 3)
+        addEdge(graph, 6, 5)
+        addEdge(graph, 3, 4)
+        addEdge(graph, 1, 0)
+        addEdge(graph, 1, 7)
+        val solver = Solver(k, isUniversity, graph)
+        assertEquals(9, solver.solve())
+    }
+
+    private fun initGraph(size: Int): List<Vertex> {
+        val graph = mutableListOf<Vertex>()
+        for (i in 0 until size) {
+            graph.add(Vertex())
+        }
+        return graph.toList()
+    }
+
+    private fun addEdge(graph: List<Vertex>, edgeA: Int, edgeB: Int) {
+        graph[edgeA].edges.add(edgeB)
+        graph[edgeB].edges.add(edgeA)
     }
 }
