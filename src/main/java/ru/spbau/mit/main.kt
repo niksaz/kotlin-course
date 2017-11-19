50c99ebc63bc1b833b592a45a3369ebe80e1dfd4
niksaz <nikitasazanovich@gmail.com>
2017-11-19 19:49:08 +0300

    Made Node immutable.

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -3,53 +3,7 @@ package ru.spbau.mit
 import java.io.*
 import java.util.*
 
-class Solver(private val universityPairs: Int, private val graph: List<Vertex>) {
-    fun solve(): Long {
-        computeSubtreeUniversityCount(graph[0])
-        val root = findOptimalVertex(graph[0])
-        return sumFromRoot(root, root, 0)
-    }
-
-    private fun computeSubtreeUniversityCount(vertex: Vertex, parent: Vertex = vertex) {
-        if (vertex.hasUniversity) {
-            vertex.subtreeUniversityCount++
-        }
-        for (toVertex in vertex.edges) {
-            if (toVertex !== parent) {
-                computeSubtreeUniversityCount(toVertex, vertex)
-                val childCount = toVertex.subtreeUniversityCount
-                vertex.subtreeUniversityCount += childCount
-                if (childCount > vertex.maxSubtreeUniversityCount?.first ?: 0) {
-                    vertex.maxSubtreeUniversityCount = Pair(childCount, toVertex)
-                }
-            }
-        }
-    }
-
-    private fun findOptimalVertex(vertex: Vertex): Vertex {
-        return if (vertex.maxSubtreeUniversityCount!!.first > universityPairs) {
-            findOptimalVertex(vertex.maxSubtreeUniversityCount!!.second)
-        } else {
-            vertex
-        }
-    }
-
-    private fun sumFromRoot(vertex: Vertex, parent: Vertex, distanceToRoot: Int): Long {
-        return (if (vertex.hasUniversity) distanceToRoot.toLong() else 0L) +
-            vertex.edges
-            .filter { it !== parent }
-            .map { sumFromRoot(it, vertex, distanceToRoot + 1) }
-            .sum()
-    }
-}
-
-data class Vertex(
-    val edges: MutableList<Vertex> = mutableListOf(),
-    var hasUniversity: Boolean = false,
-    var subtreeUniversityCount: Int = 0,
-    var maxSubtreeUniversityCount: Pair<Int, Vertex>? = null)
-
-class InputReader(stream: InputStream) {
+class BufferedScanner(stream: InputStream) {
     private val reader: BufferedReader = BufferedReader(InputStreamReader(stream), 32768)
     private var tokenizer: StringTokenizer? = null
 
@@ -63,23 +17,79 @@ class InputReader(stream: InputStream) {
     fun nextInt() = Integer.parseInt(next())
 }
 
+data class Treelandia(val nodes: List<Node>, val universityPairs: Int) {
+    val vertexes: Int
+        get() = nodes.size
+}
+
+data class Node(
+    val index: Int,
+    val hasUniversity: Boolean,
+    val edges: MutableList<Node> = mutableListOf()
+)
+
+fun readTreelandia(scanner: BufferedScanner): Treelandia {
+    val vertexNumber = scanner.nextInt()
+    val universityPairs = scanner.nextInt()
+    val hasUniversityIndexes = BooleanArray(vertexNumber)
+    repeat(2 * universityPairs) {
+        val universityIndex = scanner.nextInt() - 1
+        hasUniversityIndexes[universityIndex] = true
+    }
+    val nodes = (0 until vertexNumber).map { Node(it, hasUniversityIndexes[it]) }
+    repeat(vertexNumber - 1) {
+        val edgeA = scanner.nextInt() - 1
+        val edgeB = scanner.nextInt() - 1
+        nodes[edgeA].edges.add(nodes[edgeB])
+        nodes[edgeB].edges.add(nodes[edgeA])
+    }
+    return Treelandia(nodes, universityPairs)
+}
+
+fun computeSubtreeUniversities(subtreeUniversities: IntArray, node: Node, parent: Node = node) {
+    subtreeUniversities[node.index] = (if (node.hasUniversity) 1 else 0) +
+        node.edges
+            .filter { it !== parent }
+            .map {
+                computeSubtreeUniversities(subtreeUniversities, it, node)
+                subtreeUniversities[it.index]
+            }
+            .sum()
+}
+
+fun findOptimalVertex(
+    subtreeUniversities: IntArray,
+    universityPairs: Int,
+    node: Node,
+    parent: Node = node
+): Node {
+    val maxSubtreeChild = node.edges
+        .filter { it != parent }
+        .map { it to subtreeUniversities[it.index] }
+        .maxBy { it.second }!!
+    return if (maxSubtreeChild.second <= universityPairs) node else {
+        findOptimalVertex(subtreeUniversities, universityPairs, maxSubtreeChild.first, node)
+    }
+}
+
+fun findDistancesToUniversities(node: Node, parent: Node = node, distanceToRoot: Long = 0L): Long {
+    return (if (node.hasUniversity) distanceToRoot else 0L) +
+        node.edges
+            .filter { it !== parent }
+            .map { findDistancesToUniversities(it, node, distanceToRoot + 1L) }
+            .sum()
+}
+
+fun findMinDistancesSum(treelandia: Treelandia): Long {
+    val subtreeUniversities = IntArray(treelandia.vertexes)
+    computeSubtreeUniversities(subtreeUniversities, treelandia.nodes[0])
+    val optimalVertex =
+        findOptimalVertex(subtreeUniversities, treelandia.universityPairs, treelandia.nodes[0])
+    return findDistancesToUniversities(optimalVertex)
+}
+
 fun main(args: Array<String>) {
-    val reader = InputReader(System.`in`)
-
-    val vertexNumber = reader.nextInt()
-    val graph = List(vertexNumber, { Vertex() })
-    val universityPairs = reader.nextInt()
-    for (i in 0 until 2 * universityPairs) {
-        val universityIndex = reader.nextInt() - 1
-        graph[universityIndex].hasUniversity = true
-    }
-    for (i in 0 until vertexNumber - 1) {
-        val edgeA = reader.nextInt() - 1
-        val edgeB = reader.nextInt() - 1
-        graph[edgeA].edges.add(graph[edgeB])
-        graph[edgeB].edges.add(graph[edgeA])
-    }
-
-    val solver = Solver(universityPairs, graph)
-    println(solver.solve())
+    val scanner = BufferedScanner(System.`in`)
+    val minDistancesSum = findMinDistancesSum(readTreelandia(scanner))
+    println(minDistancesSum)
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,52 +1,46 @@
 package ru.spbau.mit
 
-import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class TestSource {
     @Test
     fun exampleTest1() {
         val vertexNumber = 7
         val universityPairs = 2
-        val graph = List(vertexNumber, { Vertex() })
-        graph[0].hasUniversity = true
-        graph[1].hasUniversity = true
-        graph[4].hasUniversity = true
-        graph[5].hasUniversity = true
-        addEdge(graph, 0, 2)
-        addEdge(graph, 2, 1)
-        addEdge(graph, 3, 4)
-        addEdge(graph, 2, 6)
-        addEdge(graph, 3, 2)
-        addEdge(graph, 3, 5)
-        val solver = Solver(universityPairs, graph)
-        assertEquals(6, solver.solve())
+        val hasUniversityIndexes = booleanArrayOf(true, true, false, false, true, true, false)
+        val nodes = (0 until vertexNumber).map { Node(it, hasUniversityIndexes[it]) }
+        addEdge(nodes, 0, 2)
+        addEdge(nodes, 2, 1)
+        addEdge(nodes, 3, 4)
+        addEdge(nodes, 2, 6)
+        addEdge(nodes, 3, 2)
+        addEdge(nodes, 3, 5)
+        val sum = findMinDistancesSum(Treelandia(nodes, universityPairs))
+        assertEquals(6L, sum)
     }
 
     @Test
     fun exampleTest2() {
         val vertexNumber = 9
         val universityPairs = 3
-        val graph = List(vertexNumber, { Vertex() })
-        graph[0].hasUniversity = true
-        graph[1].hasUniversity = true
-        graph[2].hasUniversity = true
-        graph[4].hasUniversity = true
-        graph[5].hasUniversity = true
-        graph[8].hasUniversity = true
-        addEdge(graph, 7, 8)
-        addEdge(graph, 2, 1)
-        addEdge(graph, 1, 6)
-        addEdge(graph, 2, 3)
-        addEdge(graph, 6, 5)
-        addEdge(graph, 3, 4)
-        addEdge(graph, 1, 0)
-        addEdge(graph, 1, 7)
-        val solver = Solver(universityPairs, graph)
-        assertEquals(9, solver.solve())
+        val hasUniversityIndexes = booleanArrayOf(
+            true, true, true, false, true, true, false, false, true
+        )
+        val nodes = (0 until vertexNumber).map { Node(it, hasUniversityIndexes[it]) }
+        addEdge(nodes, 7, 8)
+        addEdge(nodes, 2, 1)
+        addEdge(nodes, 1, 6)
+        addEdge(nodes, 2, 3)
+        addEdge(nodes, 6, 5)
+        addEdge(nodes, 3, 4)
+        addEdge(nodes, 1, 0)
+        addEdge(nodes, 1, 7)
+        val sum = findMinDistancesSum(Treelandia(nodes, universityPairs))
+        assertEquals(9L, sum)
     }
 
-    private fun addEdge(graph: List<Vertex>, edgeA: Int, edgeB: Int) {
+    private fun addEdge(graph: List<Node>, edgeA: Int, edgeB: Int) {
         graph[edgeA].edges.add(graph[edgeB])
         graph[edgeB].edges.add(graph[edgeA])
     }
